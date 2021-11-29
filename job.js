2d70f2104e4be95b8afbe095e287c48885e60611
RoushanFW196 <roushankumar6338@gmail.com>
2021-11-29 16:59:01 +0530

    Update job.js

--- job.js
+++ job.js
@@ -4,12 +4,17 @@ const express= require('express');
  
  const mongoose = require('mongoose');
 
+
+
+ // connecting mongoose with mongodb naukri 
  const connect= async ()=>{
      mongoose.connect("mongodb://127.0.0.1:27017/naukri")
  }
 
-app.use(express.json());
+   app.use(express.json());
 
+
+// job schema
  const jobschema= new mongoose.Schema({
 
     id:{type:Number, required:true},
@@ -30,28 +35,68 @@ app.use(express.json());
  })
 
 
- const jobs=mongoose.model("job",jobschema);
+ const jobs=mongoose.model("job",jobschema);   // jobs collection are created under naukri database
 
 
-//  app.get("/jobs",async (req, res) => {
 
-//     const alljobs= await jobs.find().lean().exec();
-//     res.send({alljobs})
+// all of the job list available on naukri database
+
+    // app.get("/jobs",async (req, res) => {
+
+    //   try{
+    //     const alljobs= await jobs.find().lean().exec();
+    //          res.status(201).send({alljobs})
+    //   }
+    //     catch(e){
+
+    //         res.status(500).send({e})
+    //     }
+    
+
+    //  })
 
 
-//  })
+
 
 
 // all jobs in particular city and with particular skills
 
 
-app.get("/jobs",async (req, res) => {
+//     app.get("/jobs",async (req, res) => {
 
-    const skilljob= await jobs.find({$or:[{"city":{$eq:"poland"}},{"skills":{$eq:"Node.js"}}]}).lean().exec();
-    res.send({skilljob})
+//     try{
 
+    
+//     const skilljob= await jobs.find({$or:[{"city":{$eq:"poland"}},{"skills":{$eq:"Node.js"}}]}).lean().exec();
+//     res.status(500).send({skilljob})
+//     }
+//     catch(e){
+//         res.status(500).send({e})
+//     }
+
+//    })
+
+
+
+   app.get("/jobs/:city",async (req, res) => {
+
+    try{
+
+    
+    const skilljob= await jobs.find({$and:[{"city":{$eq:req.params.city}},{"skills":"Node.js"}]}).lean().exec();
+    res.status(500).send({skilljob})
+    }
+    catch(e){
+        res.status(500).send({e})
+    }
 
    })
+
+
+
+
+
+
 
  // "city":"Poland","skills":"Node.js"
 
@@ -60,34 +105,55 @@ app.get("/jobs",async (req, res) => {
 
 
  // jobs available for work from home
-//    app.get("/jobs",async (req, res) => {
+   app.get("/jobs",async (req, res) => {
 
-//     const workfromhomejob= await jobs.find({"workfromhome":true}).lean().exec();
-//     res.send({workfromhomejob})
+    try{
 
-
-//    })
-
-
-//    app.get("/jobs",async (req, res) => {
-
-//     const noticeperiodjob= await jobs.find({"noticeperiod":{$eq:2}}).lean().exec();
-//     res.send({noticeperiodjob})
+    
+    const workfromhomejob= await jobs.find({"workfromhome":true}).lean().exec();
+    res.status(500).send({workfromhomejob})
+    }
+    catch(e){
+        res.status(500).send({e})
+    }
 
 
-//    })
+   })
+
+
+   // jobs which having 2 month notice periods
+
+   app.get("/jobs",async (req, res) => {
+
+    try{
+
+    
+    const noticeperiodjob= await jobs.find({"noticeperiod":{$eq:2}}).lean().exec();
+    res.send({noticeperiodjob})
+    }
+    catch(e){
+        res.status(500).send({e})
+    }
+
+
+
+   })
 
 
    // sorting the jobs as per rating 
 
 
-//    app.get("/jobs",async (req, res) => {
+   app.get("/jobs",async (req, res) => {
+       try{
+    const highratingjob= await jobs.find().sort({"rating":-1}).lean().exec();
+    res.send({highratingjob})
+       }
+       catch(e){
+        res.status(500).send({e})
+    }
 
-//     const ratingjob= await jobs.find().sort({"rating":-1}).lean().exec();
-//     res.send({ratingjob})
 
-
-//    })
+   })
 
 
 
@@ -113,10 +179,18 @@ const companyschema=new mongoose.Schema({
   const company=mongoose.model("company",companyschema);
 
 
-  // geting the list of the company
+  // geting the list of all the company
   app.get("/companies", async (req, res) => {
+       try{
+
+       
     const allcompany= await company.find().lean().exec();
     res.send({allcompany})
+       }
+       catch(e){
+        res.status(500).send({e})
+    }
+
 })
 
 
@@ -124,9 +198,17 @@ const companyschema=new mongoose.Schema({
 
 app.get("/companies/:id", async (req, res) => {
 
+      try{
+
+      
   const companydetail= await company.find({"_id":{$eq:req.params.id}}).lean().exec();
 
   res.send({companydetail})
+      }
+
+      catch(e){
+        res.status(500).send({e})
+    }
 
 
 })
@@ -137,11 +219,16 @@ app.get("/companies/:id", async (req, res) => {
 
   
 app.get("/companies", async (req, res) => {
-
-    const mostopenjobcompany= await company.find({"openjobs":{$gte:req.params.no}}).sort().lean().exec();
+      
+    try{
+  const mostopenjobcompany= await company.find().sort({"openjobs":-1}).limit(10).lean().exec();
   
     res.send({mostopenjobcompany})
-  
+    }
+    catch(e){
+        res.status(500).send({e})
+    }
+
   
   })
   
